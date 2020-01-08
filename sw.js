d7d173d434016d52579c97d5052dfd1abde2d09a
abc <abc@example.com>
2020-01-08 12:12:14 -0600

    new update

--- sw.js
+++ sw.js
@@ -1,30 +1,13 @@
-const cacheName = 'v1';
-const allFile = ['index.html', 'About.html', 'Contact.html', 'Staff.html', 'main.js', 'sw.js', 'styles/about.css', 'styles/contact.css', 'styles/header-footer.css', 'styles/index.css', 'styles/normalize.css', 'styles/staff.css', 'styles/style.css']
-self.addEventListener('install', function(e) {
+const cacheName = 'v2';
+const allFile = ['index.html', 'About.html', 'Contact.html', 'Staff.html', 'main.js','styles/about.css', 'styles/contact.css', 'styles/header-footer.css', 'styles/index.css', 'styles/normalize.css', 'styles/staff.css', 'styles/style.css']
+self.addEventListener('install', function (e) {
+  console.log('installing')
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
       return cache.addAll(allFile);
     })
       .then(() => self.skipWaiting())
   );
-});
- 
-self.addEventListener('activate', e => {
-  console.log('avtivate');
-  e.waitUntil(
-    Promise.all([ 
-      caches.keys().then(cacheNames => {
-        return Promise.all(
-          cacheNames.map(cache => {
-            if (cache !== cacheName) {
-              return caches.delete(cache);
-            }
-          })
-        )
-        }
-      )
-    ])
-  )
 });
 
 self.addEventListener('fetch', e => {
