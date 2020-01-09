1fbe126bdd36d27dcdd379a5a8806fbc36b8f800
abc <abc@example.com>
2020-01-09 01:09:05 -0600

    changed the sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,6 @@
 const cacheName = 'v2';
 const allFile = ['index.html', 'About.html', 'Contact.html', 'Staff.html', 'main.js','styles/about.css', 'styles/contact.css', 'styles/header-footer.css', 'styles/index.css', 'styles/normalize.css', 'styles/staff.css', 'styles/style.css']
 self.addEventListener('install', function (e) {
-  console.log('installing')
   e.waitUntil(
     caches.open(cacheName).then(function (cache) {
       return cache.addAll(allFile);
@@ -9,8 +8,44 @@ self.addEventListener('install', function (e) {
       .then(() => self.skipWaiting())
   );
 });
+ 
+self.addEventListener('activate', event => {
+  console.log('Activating new service worker...');
 
-self.addEventListener('fetch', e => {
-  console.log('fetch');
-  e.respondWith(fetch(e.request).catch(() => caches.match(e.request)));
+  const cacheWhitelist = [cacheName];
+
+  event.waitUntil(
+    caches.keys().then(cacheNames => {
+      return Promise.all(
+        cacheNames.map(cacheName => {
+          if (cacheWhitelist.indexOf(cacheName) === -1) {
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
+});
+
+
+self.addEventListener('fetch', event => {
+  console.log('Fetch event for ', event.request.url);
+  event.respondWith(
+    caches.match(event.request)
+    .then(response => {
+      if (response) {
+        console.log('Found ', event.request.url, ' in cache');
+        return response;
+      }
+      console.log('Network request for ', event.request.url);
+      return fetch(event.request)
+
+      // TODO 4 - Add fetched files to the cache
+
+    }).catch(error => {
+
+      // TODO 6 - Respond with custom offline page
+
+    })
+  );
 });
